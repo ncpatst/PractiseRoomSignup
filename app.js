1b958d9ee230af9992d557c0ccba9ef07ebc8666
Leon <leonluleonlu@gmail.com>
2019-11-13 13:31:40 +0800

    update read only close time

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const openHour = 07 //the hour when signup opens; 0 <= openHour <= 23
 const openMin = 00 //the minute when signup opens; 0 <= openMin <= 59
 const closeHour = 16 //the hour when signup closes; 0 <= closeHour <= 23
 const closeMin = 05 //the minute when signup closes; 0 <= closeMin <= 59
-const readOnlyHour = 23 //the hour when signup read closes; 0 <= closeHour <= 23
+const readOnlyHour = 22 //the hour when signup read closes; 0 <= closeHour <= 23
 const readOnlyMin = 30 //the minute when signup read closes; 0 <= closeMin <= 59
 
 const operationPasswordHash = "7c9646c6385ff8a32ece75e0b3ff778d007a26ca19a6d5d22bd5394d63e6ebd9"; //set password using this, only put the hash in the source code, DO NOT put anything related to the password
